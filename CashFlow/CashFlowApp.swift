ec8ec10c1508b7a07157bd82e51151953ab3b612
Debo Akeredolu <aakeredolu@apple.com>
2023-10-24 16:01:07 -0700

    Initial Commit

---
+++ CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by ADEBOLA AKEREDOLU on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
