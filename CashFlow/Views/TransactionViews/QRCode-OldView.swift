9fc70f2becb3c1001197d78f1f20cd8511177f2d
aakeredolu <aakeredolu@apple.com>
2023-10-29 10:06:15 -0700

    Receipt Sheet working

---
+++ CashFlow/Views/TransactionViews/QRCode-OldView.swift
@@ -0,0 +1,18 @@
+//
+//  QRCode-OldView.swift
+//  CashFlow
+//
+//  Created by ADEBOLA AKEREDOLU on 10/28/23.
+//
+
+import SwiftUI
+
+struct QRCode_OldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRCode_OldView()
+}
